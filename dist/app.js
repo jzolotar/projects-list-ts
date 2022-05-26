dda1d71e1f16661586b71803b9fb8a91b9dc6a21
jzolotar <zolotarjakub@gmail.com>
2022-05-26 12:00:25 +0200

    add form clear on submit, simple validation in progress

--- dist/app.js
+++ dist/app.js
@@ -12,9 +12,34 @@ class ProjectInput {
         this.configure();
         this.attach();
     }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDesc = this.descInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 ||
+            enteredDesc.trim().length === 0 ||
+            enteredPeople.trim().length === 0) {
+            alert('Invalid input, try again');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDesc, +enteredPeople];
+        }
+    }
     submitHandler(event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearForm();
+        }
+    }
+    clearForm() {
+        this.titleInputElement.value = '';
+        this.descInputElement.value = '';
+        this.peopleInputElement.value = '';
     }
     configure() {
         this.element.addEventListener('submit', this.submitHandler.bind(this));
--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ class ProjectInput {
     this.element = importedNote.firstElementChild as HTMLFormElement;
     this.element.id = 'user-input';
 
+    //access to input elements
     this.titleInputElement = this.element.querySelector(
       '#title'
     ) as HTMLInputElement;
@@ -29,14 +30,44 @@ class ProjectInput {
       '#people'
     ) as HTMLInputElement;
 
-    //access to differents elements
     this.configure();
     this.attach();
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDesc = this.descInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    //validation
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDesc.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert('Invalid input, try again');
+      return;
+    } else {
+      return [enteredTitle, enteredDesc, +enteredPeople];
+    }
   }
 
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearForm();
+    }
+  }
+
+  private clearForm() {
+    this.titleInputElement.value = '';
+    this.descInputElement.value = '';
+    this.peopleInputElement.value = '';
   }
 
   private configure() {
